6468a45e44dd964297110552888229a71e2b86ad
m0hh <lordegy53@gmail.com>
2021-03-22 02:47:05 +0200

    added custom admin add model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,26 @@
-#from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
+from django.utils.translation import gettext as _
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = [
+        (None,{'fields': ('email', 'password')} ),
+        (_('personal Info'), {'fields': ('name', )}),
+        (
+            _('permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('important dates'), {'fields': ('last_login', )})
+    ]
+    add_fieldsets = (
+        (None, {
+            'classes' :('wide',),
+            'fields' : ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin
+)
