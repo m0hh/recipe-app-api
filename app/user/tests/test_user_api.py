64390ff2a9afd23dcdb07f65249bb1143fe08223
m0hh <lordegy53@gmail.com>
2021-03-25 02:28:19 +0200

    managed user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,8 @@ CREATE_USER_URL = reverse('user:create')
 
 Token_URL = reverse('user:token')
 
+ME_URL = reverse('user:me')
+
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -81,5 +83,47 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(Token_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retreive_user_unauthorized(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateUserAPITests(TestCase):
+
+    def setUp(self):
+        self.user = create_user(
+            email= 'test@cairo.com',
+            password = 'testpass',
+            name = 'name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user = self.user)
+    
+    def test_retrieve_profile_success(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(res.data,{
+            'name': self.user.name,
+            'email': self.user.email
+        } 
+        )
+    
+    def test_post_me_not_allowed(self):
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        payload = {'name' : 'new name', 'password': 'newpassowrd123'}
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
         
     
