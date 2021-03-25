a0ba951715c07c933b544b8b5040917a595fa404
m0hh <lordegy53@gmail.com>
2021-03-25 20:09:50 +0200

    added Ingredient add feature

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -62,3 +62,22 @@ class PrivateIngredientsApiRest(TestCase):
         self.assertEqual(len(res.data), 1)
 
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredeint_successful(self):
+        payload = {'name': 'molo5ya'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+    
+    def test_create_ingredeint_invalid(self):
+        payload = {'name': ''}
+
+        res = self.client.post(INGREDIENT_URL,payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+         
